feb94e79b92689fb1e12519f7684ff8eb9ee3efb
KamiKruse <altsake@protonmail.com>
2024-11-03 13:00:14 +0530

    Create first and simple threejs scene with red spinning cube

--- Chapter 3/script.js
+++ Chapter 3/script.js
@@ -1,4 +1,36 @@
 import * as THREE from 'three';
 
-console.log("Hello Three.js");
-console.log(THREE);
+//Scene
+const scene = new THREE.Scene()
+
+//Mesh
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 5
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+const animate = () => {
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate)
+// document.body.appendChild(renderer.domElement)
