69846beb86d020db37e54fba953dbfa6458f6cc0
KamiKruse <altsake@protonmail.com>
2024-11-10 20:14:44 +0530

    Complete Chapter 4 and learnt about different transforms and grouping meshes

--- Chapter 3/Chapter 4/src/script.js
+++ Chapter 3/Chapter 4/src/script.js
@@ -5,16 +5,44 @@ import * as THREE from 'three';
 const scene = new THREE.Scene()
 
 //Mesh
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+// mesh.position.set(0.7, -0.6, 1)
+// mesh.scale.set(2,1,1)
+const group = new THREE.Group()
+scene.add(group)
+group.position.y = 1
+group.rotation.y = 2
+group.scale.y = 3
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xffff00})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+group.add(cube2)
+cube2.position.set(-2,0,0)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ffff})
+)
+group.add(cube3)
+cube3.position.set(2,0,0)
+
 
 const sizes = {
     width: 800,
     height: 600
 }
-
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper)
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
 camera.position.z = 5
